919ac1055224c7441249a4d359c959a6d17345a7
susanlovaglio <susan@flatironschool.com>
2016-07-29 14:19:06 -0400

    added project files

---
+++ ViewsInCodeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewsInCodeViewController.swift
+//  iOS-Week8-Swift-Assessment
+//
+//  Created by Flatiron School on 7/28/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewsInCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     1. Programmatically put a UIView on screen
+     2. Give it a background color so we can see it
+     3. Constrain it to be:
+     - half the height of the screen
+     - 20 points from the top
+     - 20 points from both sides
+     */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
