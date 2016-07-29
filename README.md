777fd39b018f1d645f25aff9c30611893cda93c4
susanlovaglio <susan@flatironschool.com>
2016-07-29 18:11:19 -0400

    updated readme

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 ##Instructions
 ###Views In Code
-* Create a view programatically.
-* Give it a light grey background color.
-* Set the constraints to:
+* Add a view to your view controller in storyboard.
+* Programatically set the constraints to:
 	* Height - half the height of the view
 	* 20 pts from the top
 	* 20 pts from left and right
--- iOS-Swift-Assessment-ViewsInCode/ViewsInCodeViewController.swift
+++ iOS-Swift-Assessment-ViewsInCode/ViewsInCodeViewController.swift
@@ -15,24 +15,4 @@ class ViewsInCodeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    /*
-     1. Programmatically put a UIView on screen
-     2. Give it a background color so we can see it
-     3. Constrain it to be:
-     - half the height of the screen
-     - 20 points from the top
-     - 20 points from both sides
-     */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
